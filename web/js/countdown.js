ab0b4ad826a99e6182594c9cf8eeffbb55ca2a1a
Laural-Lee Hollande <71769490+lauraleehollande@users.noreply.github.com>
2022-11-28 13:35:25 +0100

    Correction of the Christmas date

--- web/js/countdown.js
+++ web/js/countdown.js
@@ -9,7 +9,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "12/24/",
+        dayMonth = "12/25/",
         xmas = dayMonth + yyyy;
 
     today = mm + "/" + dd + "/" + yyyy;
