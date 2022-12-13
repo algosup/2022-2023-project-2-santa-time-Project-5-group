ff4d0293547e2df36686bda3a40bc57bc95332e7
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-12-13 22:23:37 +0100

    Update countdown

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -4,40 +4,40 @@
         hour = minute * 60,
         day = hour * 24;
 
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "12/25/",
-        xmas = dayMonth + yyyy;
 
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > xmas) {
-        xmas = dayMonth + nextYear;
-    }
+    let date = 0;
 
-    const countDown = new Date(xmas).getTime(),
-        x = setInterval(function () {
+    fetch("https://worldtimeapi.org/api/timezone/Europe/London")
+    .then(r => r.json())
+    .then(data => {
+        let now = new Date(data.datetime);
+        date = Date.parse(now);
+        // console.log(date);
+    
+    });
 
-            const now = new Date().getTime(),
-                distance = countDown - now + (10*offset);
+    const countDown =  	1671926400000; // 12/25/2022 at 00:00:00 UTC
 
-            document.getElementById("days").innerText = Math.floor(distance / (day)).toLocaleString(undefined, { minimumIntegerDigits: 2 }),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)).toLocaleString(undefined, { minimumIntegerDigits: 2 }),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)).toLocaleString(undefined, { minimumIntegerDigits: 2 }),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second).toLocaleString(undefined, { minimumIntegerDigits: 2 });
 
-            if (distance < 0) {
-                document.getElementById("headline").innerText = "It's christmas!";
-                document.getElementById("countdown").style.display = "none";
-                clearInterval(x);
-            }
+    x = setInterval(function () {
+        let distance = countDown - date + (10*offset)  ;
 
-            if (offset != 0) {
-                let timer = 'XMAS : ' + (Math.floor(distance / (day)).toLocaleString(undefined, { minimumIntegerDigits: 2 })) + 'd ' + (Math.floor((distance % (day)) / (hour)).toLocaleString(undefined, { minimumIntegerDigits: 2 })) + 'h - ' + (Math.floor((distance % (hour)) / (minute)).toLocaleString(undefined, { minimumIntegerDigits: 2 })) + ':' + (Math.floor((distance % (minute)) / second).toLocaleString(undefined, { minimumIntegerDigits: 2 }));
-                document.title = timer;
-            }
-            //seconds
-        }, 0)
+        document.getElementById("days").innerText = Math.floor(distance / (day)).toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)).toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)).toLocaleString(undefined, { minimumIntegerDigits: 2 }),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second).toLocaleString(undefined, { minimumIntegerDigits: 2 });
+
+        if (distance < 0) {
+            document.getElementById("headline").innerText = "It's christmas!";
+            document.getElementById("countdown").style.display = "none";
+            clearInterval(x);
+        }
+
+        if (offset != 0) {
+            let timer = 'XMAS : ' + (Math.floor(distance / (day)).toLocaleString(undefined, { minimumIntegerDigits: 2 })) + 'd ' + (Math.floor((distance % (day)) / (hour)).toLocaleString(undefined, { minimumIntegerDigits: 2 })) + 'h - ' + (Math.floor((distance % (hour)) / (minute)).toLocaleString(undefined, { minimumIntegerDigits: 2 })) + ':' + (Math.floor((distance % (minute)) / second).toLocaleString(undefined, { minimumIntegerDigits: 2 }));
+            document.title = timer;
+        }
+        date += 1000;
+        //seconds
+    }, 1000)
 }());
